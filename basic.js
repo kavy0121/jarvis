95451b233618048eb19c664b3c17f06b262af501
kavy0121 <kavy0121@gmail.com>
2025-11-01 11:45:47 +0530

    js basics

---
+++ basic.js
@@ -0,0 +1,18 @@
+console.log("hello marvel");
+
+let namee="Tony STARK";
+let age=22;
+
+console.log(namee);
+console.log(age);
+
+console.log(namee,age);
+console.table([namee,age])
+
+console.log(typeof age);
+
+let a=20;
+let b="20";
+
+console.log(a==b);
+console.log(a===b);
